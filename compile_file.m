0dbbbdaf85b9d8c8e7a35b4fb7c5991a64d761c7
Minghao Guo <mh.guo0111@gmail.com>
2017-10-30 14:43:00 -0700

    projector for single angle

--- compile_file.m
+++ compile_file.m
@@ -1,6 +1,6 @@
 close all;clc
 
-!nvcc -shared -c -Xcompiler -fPIC kernel_backprojection.cu -I/usr/local/MATLAB/R2017b/extern/include 
+% !nvcc -shared -c -Xcompiler -fPIC kernel_backprojection.cu -I/usr/local/MATLAB/R2017b/extern/include 
     
 if 0
     !nvcc -shared -c -Xcompiler -fPIC kernel_add.cu -I/usr/local/MATLAB/R2017b/extern/include 
@@ -14,11 +14,15 @@ if 0
     !nvcc -shared -c -Xcompiler -fPIC kernel_invertDVF.cu -I/usr/local/MATLAB/R2017b/extern/include 
 end
 
-if 01
+if 0
     mexcuda SART_cuda.cu kernel_add.o kernel_backprojection.o kernel_deformation.o kernel_division.o kernel_initial.o...
     kernel_projection.o kernel_update.o kernel_forwardDVF.o kernel_invertDVF.o
 end
 
 if 0
     mexcuda generateProj_mex.cu kernel_deformation.o kernel_projection.o kernel_forwardDVF.o kernel_invertDVF.o
+end
+
+if 1
+    mexcuda host_projection.cu kernel_projection.o
 end
