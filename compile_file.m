5d836f459485619a2409088b33c9411b52ca0d30
Minghao Guo <mh.guo0111@gmail.com>
2017-10-30 13:54:55 -0700

    generator for projection

--- compile_file.m
+++ compile_file.m
@@ -1,3 +1,24 @@
 close all;clc
-mexcuda SART_cuda.cu kernel_add.cu kernel_backprojection.cu kernel_deformation.cu kernel_division.cu kernel_initial.cu...
-kernel_projection.cu kernel_update.cu kernel_forwardDVF.cu kernel_invertDVF.cu
+
+!nvcc -shared -c -Xcompiler -fPIC kernel_backprojection.cu -I/usr/local/MATLAB/R2017b/extern/include 
+    
+if 0
+    !nvcc -shared -c -Xcompiler -fPIC kernel_add.cu -I/usr/local/MATLAB/R2017b/extern/include 
+    !nvcc -shared -c -Xcompiler -fPIC kernel_deformation.cu -I/usr/local/MATLAB/R2017b/extern/include 
+    !nvcc -shared -c -Xcompiler -fPIC kernel_backprojection.cu -I/usr/local/MATLAB/R2017b/extern/include 
+    !nvcc -shared -c -Xcompiler -fPIC kernel_division.cu -I/usr/local/MATLAB/R2017b/extern/include 
+    !nvcc -shared -c -Xcompiler -fPIC kernel_initial.cu -I/usr/local/MATLAB/R2017b/extern/include 
+    !nvcc -shared -c -Xcompiler -fPIC kernel_projection.cu -I/usr/local/MATLAB/R2017b/extern/include 
+    !nvcc -shared -c -Xcompiler -fPIC kernel_update.cu -I/usr/local/MATLAB/R2017b/extern/include 
+    !nvcc -shared -c -Xcompiler -fPIC kernel_forwardDVF.cu -I/usr/local/MATLAB/R2017b/extern/include 
+    !nvcc -shared -c -Xcompiler -fPIC kernel_invertDVF.cu -I/usr/local/MATLAB/R2017b/extern/include 
+end
+
+if 01
+    mexcuda SART_cuda.cu kernel_add.o kernel_backprojection.o kernel_deformation.o kernel_division.o kernel_initial.o...
+    kernel_projection.o kernel_update.o kernel_forwardDVF.o kernel_invertDVF.o
+end
+
+if 0
+    mexcuda generateProj_mex.cu kernel_deformation.o kernel_projection.o kernel_forwardDVF.o kernel_invertDVF.o
+end
